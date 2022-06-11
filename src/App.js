1c283ec6818220cc6913965c3ca77987a46934c7
tanveerns <tanveeralam229@gmail.com>
2022-06-11 18:17:07 +0530

    'react-app-setup'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import Navbar from './components/header/Navbar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
+      <h1 className="text-5xl text-white">Hello Tailwind 👋</h1>
+      <p className="text-gray-400 mt-5 text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+        consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
+        eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
+      </p>
+      <button className="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+        Hello Friends 🚀
+      </button>
+    </div>
     </div>
   );
 }
